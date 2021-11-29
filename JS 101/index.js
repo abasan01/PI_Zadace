133738ac56e5bf8d1b2436a0a066f5eba7c19794
abasan01 <andrija3000@gmail.com>
2021-11-29 20:36:39 +0100

    Gotov

--- JS 101/index.js
+++ JS 101/index.js
@@ -1,16 +1,35 @@
-const http = require('http');
+const http = require('https');
 
 const hostname = '127.0.0.1'
 const port = 3000;
+
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
     res.end('Hello World!');
+    
 });
 
 server.listen(port, hostname, () => {
 
-    console.log('Server started on port '+port);
+    let total = 0;
+    let count = 1;
+    var n = 1000; //1. Prvi zadatak varijabla
+    console.log('Varijabla: '+n);
+  
+    while (n!=0) {
+        total += count;
+        count += 1;
+        n -= 1;
+    }
+  
+    console.log('Suma zbrajanja: '+total);
 
+    var x = 5;
+
+    console.log('Zbroj prvih '+x +' brojeva = ' +((x/2)*(1+x)));
+    
 });
+
